1c7a4af2b498bccc365994959775f5ed6a6be31d
aashishgusai <45625653+aashishgusai@users.noreply.github.com>
2019-01-11 18:42:03 +0530

    Update db_conf.py

--- DonorsChooseCode/db_conf.py
+++ DonorsChooseCode/db_conf.py
@@ -2,11 +2,9 @@ import yaml
 import yaycl_crypt
 import yaycl
 
-conf = yaycl.Config('/home/ashishgusai/assesment2/code_github/', crypt_key='/home/ashishgusai/assesment2/code_github/secret_text')
+conf = yaycl.Config('/Internship_assessments/DonorsChooseCode/', crypt_key='/Internship_assessments/DonorsChooseCode/secret_text')
 #yaycl_crypt.encrypt_yaml(conf, 'config')
 yaycl_crypt.decrypt_yaml(conf, 'config')
-file1=open("/home/ashishgusai/assesment2/code_github/config.yaml")
+file1=open("/Internship_assessments/DonorsChooseCode/config.yaml")
 yaycl_crypt.encrypt_yaml(conf, 'config')
 cfg=yaml.load(file1)
-
-
