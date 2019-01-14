5621ce5b36e006895a3cb17c64af473a4375b296
aashishgusai <45625653+aashishgusai@users.noreply.github.com>
2019-01-14 19:31:33 +0530

    Update branch_python_op.py

--- assessment-2/BranchPythonOperator/branch_python_op.py
+++ assessment-2/BranchPythonOperator/branch_python_op.py
@@ -1,4 +1,5 @@
-import random
+#based on time one of listed task will run 
+#for more see the image attached with content
 import airflow
 from airflow.models import DAG
 from datetime import datetime, timedelta
@@ -7,8 +8,10 @@ from airflow.operators.python_operator import BranchPythonOperator
 
 args ={
 	'owner':'airflow',
-	'start_date':datetime(2019,1,2),
+	'start_date':datetime(2019,1,14),
 }
+#fetch current hour
+curr_time=int(datetime.now().strftime('%H'))
 
 dag = DAG(dag_id='BranchPython_example',default_args=args)
 
@@ -17,20 +20,22 @@ run_this_first = DummyOperator(
 	dag=dag
 )
 
-options = ['branch_a','branch_b','branch_c','branch_d']
+options = ['branch_a-10_15','branch_b-16_20','branch_c-21__','branch_d-last']
 
-def func_condition():
-	return lambda: random.choice(options)
-
-
-def func_condition1(options):
-	if("branch_c" in options):
+def func_condition():	#compair hours
+	if(curr_time > 10 and curr_time <= 15): 		
+		return options[0]	
+	elif(curr_time > 15 and curr_time <= 20):
+		return options[1]
+	elif(curr_time > 20):
 		return options[2]
+	else:
+		return options[3]
+
 
 branching = BranchPythonOperator(
 	task_id='branching',
-	python_callable=func_condition1,
-	op_kwargs={'options':options},	
+	python_callable=func_condition,
 	dag=dag
 )
 
@@ -54,4 +59,4 @@ for option in options:
 	)
 
 	branching >> t >> dummy_follow >> join
-	
+
