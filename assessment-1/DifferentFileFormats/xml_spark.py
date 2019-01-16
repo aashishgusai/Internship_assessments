066f0fe42b9e8347d113eb7121b60d5c6c88f5b6
aashishgusai <45625653+aashishgusai@users.noreply.github.com>
2019-01-16 14:40:09 +0530

    Update xml_spark.py

--- assessment-1/DifferentFileFormats/xml_spark.py
+++ assessment-1/DifferentFileFormats/xml_spark.py
@@ -5,7 +5,7 @@ from pyspark.sql.session import SparkSession
 sc = SparkContext(master='local[2]')
 spark=SparkSession(sc)
 #required external package for XML operation
-df = spark.read.format('com.databricks.spark.xml').options(rowTag='T').load('hdfs:///inputs/partsupp.xml')
+df = spark.read.format('com.databricks.spark.xml').options(rowTag='T').load('hdfs:///inputs/part_supply.xml')
 
 # df.show()
 
